3198030782573962bba3dca6f1ab6b4af9b84b8e
runjp777 <140734337+runjp777@users.noreply.github.com>
2023-07-27 22:37:18 +0700

    app.js

---
+++ www/app.js
@@ -0,0 +1,6 @@
+api                                 # API folder
+└─── app.js                         # API entry point
+     └─── configs                   # Configuration related stuff
+     └─── controllers               # Controllers for all the endpoints
+     └─── models                    # Object Models for connecting to database
+     └─── routes                    # Routes for all the endpoints
